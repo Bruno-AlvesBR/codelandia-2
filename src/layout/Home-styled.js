440b2531c6f2ba8ce96c616990c1b30fef46faf5
Bruno <brunoph.faces12@gmail.com>
2022-01-11 18:53:30 -0300

    adicionado renderização dos cards com responsividade

--- src/layout/Home-styled.js
+++ src/layout/Home-styled.js
@@ -70,6 +70,45 @@ export const ContainerCards = styled.section`
     p 
     {
         font-size: 1rem;
+        line-height: 20px;
         text-align: center;
+    }
+    .box-products
+    {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        margin: 50px 0;
+        .card 
+        {
+            width: 250px;
+            height: 250px;
+            background-color: #EBE9EA;
+            margin: 10px 5px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transition: .2s all ease;
+            &:hover 
+            {
+                transform: scale(1.02);
+                box-shadow: 0px 0px 10px #00000050;
+            }
+        }
+    }
+    @media (max-width: 400px)
+    {
+        .box-products
+        {
+            .card 
+            {
+                width: 100%;
+                img 
+                {
+                    width: 100%;
+                }
+            }
+        }
     }
 `;
